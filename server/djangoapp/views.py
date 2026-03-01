9a6f6e32677984bd7d88beab0bb35faef893a2a3
joec11 <joseph.candela0@gmail.com>
2026-02-28 19:18:56 -0500

    3 - Python fixes for linting.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -11,19 +11,20 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
     user = authenticate(username=username, password=password)
-    
+
     if user is not None:
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
     else:
         data = {"userName": username, "status": "Not authenticated"}
-    
+
     return JsonResponse(data)
 
 
@@ -40,20 +41,31 @@ def registration(request):
     first_name = data['firstName']
     last_name = data['lastName']
     email = data['email']
-    
+
     username_exist = False
     try:
         User.objects.get(username=username)
         username_exist = True
     except User.DoesNotExist:
         logger.debug(f"{username} is a new user")
-    
+
     if not username_exist:
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        user = User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            password=password,
+            email=email
+        )
         login(request, user)
         return JsonResponse({"userName": username, "status": "Authenticated"})
     else:
-        return JsonResponse({"userName": username, "error": "Already Registered"})
+        return JsonResponse(
+            {
+                "userName": username,
+                "error": "Already Registered"
+            }
+        )
 
 
 def get_cars(request):
@@ -61,7 +73,12 @@ def get_cars(request):
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
+    cars = [
+        {
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        } for car_model in car_models
+    ]
     return JsonResponse({"CarModels": cars})
 
 
@@ -76,7 +93,9 @@ def get_dealer_reviews(request, dealer_id):
         endpoint = f"/fetchReviews/dealer/{dealer_id}"
         reviews = get_request(endpoint)
         for review_detail in reviews:
-            review_detail['sentiment'] = analyze_review_sentiments(review_detail['review'])['sentiment']
+            review_detail['sentiment'] = analyze_review_sentiments(
+                review_detail['review']
+            )['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     return JsonResponse({"status": 400, "message": "Bad Request"})
 
@@ -97,5 +116,10 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception as e:
             logger.error(f"Error posting review: {e}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {
+                    "status": 401,
+                    "message": "Error in posting review"
+                }
+            )
     return JsonResponse({"status": 403, "message": "Unauthorized"})
